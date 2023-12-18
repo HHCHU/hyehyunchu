8e075f3704e208262796f904d60d6d69f3cacfd3
HHCHU <ded06031@snu.ac.kr>
2023-12-18 17:34:58 +0900

    🥹 Delete one more useless import

--- src/components/Layout/Page.tsx
+++ src/components/Layout/Page.tsx
@@ -1,6 +1,5 @@
 import { NextPage } from "next"
 import Head from "next/head"
-import { useRouter } from "next/router"
 import { memo, PropsWithChildren } from "react"
 
 import { HomepageMeta } from "../../data/dataDef"
